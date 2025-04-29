ca692e512321f74c63a321c3dd4df3d78a480ec8
Marat Gumerov <marat.gumerof@aspose.com>
2025-04-29 17:08:33 +0800

    Unnecessary messages removed

--- scripts/binder.sh
+++ scripts/binder.sh
@@ -16,28 +16,21 @@ find_bindings() {
       project_name=$(basename "$file" .yml | sed 's/\.yaml$//')
       BINDINGS["$key"]="$project_name"
       bb[$key]="$project_name"
-      echo "Binded $key to $project_name"
     fi
   done
-  echo "Bindings: ${!BINDINGS[@]}"
 }
 
 # Function to handle the key input and session management
 handle_key_input() {
   local pressed_key="$1"
-  echo "Pressed key is '$pressed_key'"
-  echo "Bindings: ${!BINDINGS[@]}"
 
   local session_name="${BINDINGS[$pressed_key]}"
-  echo "Session name is $session_name"
   if [[ -z "$session_name" ]]; then
     tmux display-message "No tmuxinator project bound to key '$pressed_key'."
   elif tmux has-session -t "$session_name" 2>/dev/null; then
     tmux switch-client -t "$session_name"
-    tmux display-message "Switched to session: $session_name"
   else
     tmuxinator start "$session_name"
-    tmux display-message "Started session: $session_name"
   fi
 }
 
